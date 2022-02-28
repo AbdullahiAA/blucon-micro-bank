2ba12128e0007151594fdaaf3472e8087b967a0b
Jelili Abdullahi A <osuolaleabdullahi@gmail.com>
2022-02-28 11:36:41 +0100

    keyServices Completed

--- src/components/home/KeyServices.jsx
+++ src/components/home/KeyServices.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import "./KeyServices.css"
+import contrIcon from "../../images/icons/contribution.svg"
+import bankIcon from "../../images/icons/bank.svg"
+import creditIcon from "../../images/icons/credit.svg"
+import epuipIcon from "../../images/icons/equipment.svg"
+import farmIcon from "../../images/icons/farm.svg"
+import financeIcon from "../../images/icons/finance.svg"
 
 function KeyServices() {
   return (
@@ -11,35 +17,51 @@ function KeyServices() {
 
         <div className="keyServices__list">
             <div className="keyServices__listItem">
-                <div className="keyServices__listIcon"></div>
+                <div className="keyServices__listIcon">
+                    <img src={contrIcon} alt="Daily Thrift Contribution" />
+                </div>
                 <h5 className="keyServices__listTitle">Daily Thrift Contribution</h5>
                 <p className="keyServices__listContent">Lorem ipsum dolor sit amet, consectetur adipis cing elit. Nibh nisi vel nullam lacus, velit. Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. </p>
             </div>
             <div className="keyServices__listItem">
-                <div className="keyServices__listIcon"></div>
+                <div className="keyServices__listIcon">
+                    <img src={bankIcon} alt="Bank Account" />
+                </div>
                 <h5 className="keyServices__listTitle">Bank Account</h5>
                 <p className="keyServices__listContent">Lorem ipsum dolor sit amet, consectetur adipis cing elit. Nibh nisi vel nullam lacus, velit. Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. </p>
             </div>
             <div className="keyServices__listItem">
-                <div className="keyServices__listIcon"></div>
+                <div className="keyServices__listIcon">
+                    <img src={creditIcon} alt="Credit Facilities" />
+                </div>
                 <h5 className="keyServices__listTitle">Credit Facilities</h5>
                 <p className="keyServices__listContent">Lorem ipsum dolor sit amet, consectetur adipis cing elit. Nibh nisi vel nullam lacus, velit. Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. </p>
             </div>
             <div className="keyServices__listItem">
-                <div className="keyServices__listIcon"></div>
+                <div className="keyServices__listIcon">
+                    <img src={epuipIcon} alt="Equipment Microleasing" />
+                </div>
                 <h5 className="keyServices__listTitle">Equipment Microleasing</h5>
                 <p className="keyServices__listContent">Lorem ipsum dolor sit amet, consectetur adipis cing elit. Nibh nisi vel nullam lacus, velit. Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. </p>
             </div>
             <div className="keyServices__listItem">
-                <div className="keyServices__listIcon"></div>
+                <div className="keyServices__listIcon">
+                    <img src={farmIcon} alt="Farm Input Loan" />
+                </div>
                 <h5 className="keyServices__listTitle">Farm Input Loan</h5>
                 <p className="keyServices__listContent">Lorem ipsum dolor sit amet, consectetur adipis cing elit. Nibh nisi vel nullam lacus, velit. Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. </p>
             </div>
             <div className="keyServices__listItem">
-                <div className="keyServices__listIcon"></div>
+                <div className="keyServices__listIcon">
+                    <img src={financeIcon} alt="Tricycle Finance" />
+                </div>
                 <h5 className="keyServices__listTitle">Tricycle Finance</h5>
                 <p className="keyServices__listContent">Lorem ipsum dolor sit amet, consectetur adipis cing elit. Nibh nisi vel nullam lacus, velit. Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. </p>
             </div>
+        </div>
+
+        <div className="keyServices__exploreBtn">
+            <button>Explore Our Services</button>
         </div>
     </section>
   )
