fe15cf9276bc5fd0b5b9ee97f236fd69a7df51d7
Jelili Abdullahi A <osuolaleabdullahi@gmail.com>
2022-03-02 20:48:55 +0100

    Form Stepper Added

--- src/components/accountForm/ProgressBar.jsx
+++ src/components/accountForm/ProgressBar.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProgressBar.css";
 
-function ProgressBar() {
-  return <div>ProgressBar</div>;
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import routeParams from "../../routeParams";
+import { Link, useLocation } from "react-router-dom";
+
+const steps = [
+  { label: "Bank Account Validation", page: routeParams.bankInfo },
+  { label: "Personal Information", page: routeParams.personalInfo },
+  { label: "Employer Information", page: routeParams.employerInfo },
+  { label: "Next of Kin", page: routeParams.nextOfKin },
+  { label: "Other Information", page: routeParams.otherInfo },
+];
+
+function ProgressBar({}) {
+  const { pathname } = useLocation();
+
+  function getActiveStep() {
+    switch (pathname) {
+      case routeParams.bankInfo:
+        return 1;
+      case routeParams.personalInfo:
+        return 2;
+      case routeParams.employerInfo:
+        return 3;
+      case routeParams.nextOfKin:
+        return 4;
+      case routeParams.otherInfo:
+        return 5;
+
+      default:
+        return 1;
+    }
+  }
+
+  return (
+    <Box className="progressBar" sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={getActiveStep() - 1} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={index}>
+            <Link to={step.page}>
+              <StepLabel style={{ cursor: "pointer" }}>{step.label}</StepLabel>
+            </Link>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
 }
 
 export default ProgressBar;
