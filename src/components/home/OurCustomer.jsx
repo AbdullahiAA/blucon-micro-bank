ef4bd6936a5883603538d2e01445a6803efde1a0
Jelili Abdullahi A <osuolaleabdullahi@gmail.com>
2022-03-03 15:11:57 +0100

    OurCustomer made responsive for 900px

--- src/components/home/OurCustomer.jsx
+++ src/components/home/OurCustomer.jsx
@@ -1,75 +1,58 @@
-import React from 'react'
-import "./OurCustomer.css"
-import checkIcon from "../../images/icons/checkIcon.svg"
-import customerOne from "../../images/customerOne.png"
-import customerTwo from "../../images/customerTwo.png"
-import customerThree from "../../images/customerThree.png"
+import React from "react";
+import "./OurCustomer.css";
+import checkIcon from "../../images/icons/checkIcon.svg";
+import customerOne from "../../images/customerOne.png";
+import customerTwo from "../../images/customerTwo.png";
+import customerThree from "../../images/customerThree.png";
+
+const customerCategory = [
+  "Micro-entreprenuers",
+  "Self-employed/Income Earners",
+  "Smallholder Farmers",
+  "Cooperatives",
+  "Artisan",
+  "Organised groups & Association",
+];
 
 function OurCustomer() {
   return (
-    <section className='ourCustomer'>
-        <div className="ourCustomer__header">
-            <h2>Our Customer Base</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nisi vel nullam lacus, velit. Mattis massa odio nisl gravida Lorem</p>
+    <section className="ourCustomer">
+      <div className="ourCustomer__header">
+        <h2>Our Customer Base</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nisi vel
+          nullam lacus, velit. Mattis massa odio nisl gravida Lorem
+        </p>
+      </div>
+
+      <div className="ourCustomer__content">
+        <div className="ourCustomer__images">
+          <div className="ourCustomer__imageOne">
+            <img src={customerOne} alt="Customer Img" />
+          </div>
+          <div className="ourCustomer__imageTwo">
+            <img src={customerTwo} alt="Customer Img" />
+          </div>
+          <div className="ourCustomer__imageThree">
+            <img src={customerThree} alt="Customer Img" />
+          </div>
         </div>
 
-        <div className="ourCustomer__content">
-            <div className="ourCustomer__images">
-                {/* TODO: Add the tooltip */}
-                <div className="ourCustomer__imageOne">
-                    <img src={customerOne} alt="Customer Img" />
+        <div className="ourCustomer__list">
+          <ul>
+            {customerCategory.map((category) => (
+              <li>
+                <div className="ourCustomer__listCheck">
+                  <img src={checkIcon} alt="Check" />
                 </div>
-                <div className="ourCustomer__imageTwo">
-                    <img src={customerTwo} alt="Customer Img" />
-                </div>
-                <div className="ourCustomer__imageThree">
-                    <img src={customerThree} alt="Customer Img" />
-                </div>
-            </div>
-
-            <div className="ourCustomer__list">
-                <ul>
-                    <li>
-                        <div className="ourCustomer__listCheck">
-                            <img src={checkIcon} alt="Check" />
-                        </div>
-                        <span>Micro-entreprenuers</span>
-                    </li>
-                    <li>
-                        <div className="ourCustomer__listCheck">
-                            <img src={checkIcon} alt="Check" />
-                        </div>
-                        <span>Self-employed/Income Earners</span>
-                    </li>
-                    <li>
-                        <div className="ourCustomer__listCheck">
-                            <img src={checkIcon} alt="Check" />
-                        </div>
-                        <span>Smallholder Farmers</span>
-                    </li>
-                    <li>
-                        <div className="ourCustomer__listCheck">
-                            <img src={checkIcon} alt="Check" />
-                        </div>
-                        <span>Cooperatives</span>
-                    </li>
-                    <li>
-                        <div className="ourCustomer__listCheck">
-                            <img src={checkIcon} alt="Check" />
-                        </div>
-                        <span>Artisan</span>
-                    </li>
-                    <li>
-                        <div className="ourCustomer__listCheck">
-                            <img src={checkIcon} alt="Check" />
-                        </div>
-                        <span>Organised groups & Association</span>
-                    </li>
-                </ul>
-            </div>
+                <span>{category}</span>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default OurCustomer
+export default OurCustomer;
