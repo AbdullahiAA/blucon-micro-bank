a98624b28163eca6615cb506d524b414b9ee2867
Jelili Abdullahi A <osuolaleabdullahi@gmail.com>
2022-03-04 08:27:22 +0100

    AccountType made responsive for 900px

--- src/components/openAccount/AccountType.jsx
+++ src/components/openAccount/AccountType.jsx
@@ -3,81 +3,66 @@ import { Link } from "react-router-dom";
 import routeParams from "../../routeParams";
 import "./AccountType.css";
 
+const accountType = [
+  {
+    id: 1,
+    type: "Current Account",
+    descr:
+      "Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit. Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis. velit",
+  },
+  {
+    id: 2,
+    type: "Corporate Account",
+    descr:
+      "Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit. Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis. velit",
+    isBtnReverse: true,
+  },
+  {
+    id: 3,
+    type: "Salary Account",
+    descr:
+      "Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit. Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis. velit",
+    isBtnReverse: true,
+  },
+  {
+    id: 4,
+    type: "Savings Account",
+    descr:
+      "Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit. Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis. velit",
+    isBtnReverse: true,
+  },
+  {
+    id: 5,
+    type: "Bullet Account",
+    descr:
+      "Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit. Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis. velit",
+    isBtnReverse: true,
+  },
+  {
+    id: 6,
+    type: "Children Education Account",
+    descr:
+      "Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit. Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis. velit",
+    isBtnReverse: true,
+  },
+];
+
 function AccountType() {
   return (
     <div className="accountType">
-      <div className="accountType__item">
-        <h5>Current Account</h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit.
-          Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl
-          gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis.
-          velit
-        </p>
-        <Link to={routeParams.bankInfo}>
-          <button>Select Account</button>
-        </Link>
-      </div>
-      <div className="accountType__item">
-        <h5>Corporate Account</h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit.
-          Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl
-          gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis.
-          velit
-        </p>
-        <Link to={routeParams.bankInfo}>
-          <button className="btnReverse">Select Account</button>
-        </Link>
-      </div>
-      <div className="accountType__item">
-        <h5>Salary Account</h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit.
-          Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl
-          gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis.
-          velit
-        </p>
-        <Link to={routeParams.bankInfo}>
-          <button className="btnReverse">Select Account</button>
-        </Link>
-      </div>
-      <div className="accountType__item">
-        <h5>Savings Account</h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit.
-          Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl
-          gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis.
-          velit
-        </p>
-        <Link to={routeParams.bankInfo}>
-          <button className="btnReverse">Select Account</button>
-        </Link>
-      </div>
-      <div className="accountType__item">
-        <h5>Bullet Account</h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit.
-          Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl
-          gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis.
-          velit
-        </p>
-        <Link to={routeParams.bankInfo}>
-          <button className="btnReverse">Select Account</button>
-        </Link>
-      </div>
-      <div className="accountType__item">
-        <h5>Children Education Account</h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur phasellus adipiscing elit.
-          Nibh nisi vel nullam lacus, velit velit tortor Mattis massa odio nisl
-          gravida in phasellus consectetur tortor, nisl. Dui praesent elit quis.
-          velit
-        </p>
-        <Link to={routeParams.bankInfo}>
-          <button className="btnReverse">Select Account</button>
-        </Link>
-      </div>
+      {accountType.map(({ id, type, descr, isBtnReverse }) => (
+        <div key={id} className="accountType__item">
+          <h5>{type}</h5>
+
+          <p>{descr}</p>
+
+          <Link to={routeParams.bankInfo}>
+            <button className={isBtnReverse ? "btnReverse" : ""}>
+              Select Account
+            </button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
